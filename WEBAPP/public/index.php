f7a5b80611e7ec32ad43384108ae3e148cfee67e
michalmpeis <58303680+michalmpeis@users.noreply.github.com>
2021-02-03 18:43:08 +0200

    Commiting work that has been done. Contains API and WEBAPP
    For this commit im including the completed API and im adding a basic webapp that needs some code to be finished.

---
+++ WEBAPP/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<head>
+    <title>Crime Offences</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/WEBAPP/assets/css/styling.css">
+</head>
+
+<body>
+<div class="body">
+    <img src="WEBAPP/assets/ico/background.jpg"/>
+    <p>Crime Offences</p>
+</div>
+</body>
